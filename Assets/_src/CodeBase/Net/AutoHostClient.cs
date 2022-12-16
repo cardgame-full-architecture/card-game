516c9bd8a9720d8417082d1ebce66049c0013787
Artem <atyom.najdanov@mail.ru>
2022-12-16 15:19:02 +0500

    added _src, code clean up done

---
+++ Assets/_src/CodeBase/Net/AutoHostClient.cs
@@ -0,0 +1,23 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace _src.CodeBase.Net {
+    public class AutoHostClient : MonoBehaviour {
+
+        [SerializeField] NetworkManager networkManager;
+
+        void Start () {
+            if (!Application.isBatchMode) { //Headless build
+                Debug.Log ($"=== Client Build ===");
+                networkManager.StartClient ();
+            } else {
+                Debug.Log ($"=== Server Build ===");
+            }
+        }
+
+        public void JoinLocal () {
+            networkManager.networkAddress = "localhost";
+            networkManager.StartClient ();
+        }
+    }
+}
---
+++ Assets/_src/CodeBase/UI/UIPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _src.CodeBase.UI {
+
+    public class UIPlayer : MonoBehaviour {
+
+        [SerializeField] Text text;
+        GameLogic.Player player;
+
+        public void SetPlayer (GameLogic.Player player) {
+            this.player = player;
+            text.text = "Player " + player.playerIndex.ToString ();
+        }
+
+    }
+}
